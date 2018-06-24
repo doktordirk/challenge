913aa79cd616accc777d8d39746fac899e5d743a
doktordirk <doktordirk@outlook.de>
2018-06-24 17:07:36 +0200

    chore(events): move click-enter event to events folder

--- src/resources/index.js
+++ src/resources/index.js
@@ -2,7 +2,7 @@ import {ActionConfirm} from './elements/action-confirm';
 import {Checkbox} from './elements/checkbox';
 
 // add click-enter event binding functionality
-import './attributes/click-enter';
+import './events/click-enter';
 
 export function configure(config) {
   config.globalResources([
